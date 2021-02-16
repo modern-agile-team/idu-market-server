834b097680fa641f0ce805767cded45118166efb
박우림 <woorimprog@gmail.com>
2021-02-17 00:12:45 +0900

    Implement(woorim/login) : 로그인 API 구현 완료 #2

--- app/src/apis/root/root.ctrl.js
+++ app/src/apis/root/root.ctrl.js
@@ -1,15 +1,17 @@
 "use strict";
 
+const User = require("../../models/services/User");
+
 const process = {
-  login: (req, res) => {
-    const user = new User(req);
-    const response = user.login();
+  login: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.login();
     return res.json(response);
   },
-  signup: () => {},
-  sendEmailForId: () => {},
-  sendEmailForPsword: () => {},
-  resetPsword: () => {},
+  signup: async () => {},
+  sendEmailForId: async () => {},
+  sendEmailForPsword: async () => {},
+  resetPsword: async () => {},
 };
 
 module.exports = {
