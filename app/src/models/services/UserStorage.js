75fd677c6ab64322c30c238f8009dd361722bc38
박우림 <woorimprog@gmail.com>
2021-02-17 02:05:19 +0900

    Add(woorim/signup) : UserStorage의 save 메서드 구현 #3

--- app/src/models/services/UserStorage.js
+++ app/src/models/services/UserStorage.js
@@ -5,7 +5,7 @@ const db = require("../../config/db");
 class UserStorage {
   static getUserInfo(id) {
     return new Promise((resolve, reject) => {
-      const query = "SELECT * FROM users WHERE id = ?;";
+      const query = "SELECT * FROM students WHERE id = ?;";
       db.query(query, [id], (err, data) => {
         if (err) reject(`${err}`);
         else resolve(data[0]);
@@ -13,13 +13,18 @@ class UserStorage {
     });
   }
 
-  static async save(userInfo) {
+  static async save(user) {
     return new Promise((resolve, reject) => {
-      const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
-      db.query(query, [userInfo.id, userInfo.name, userInfo.psword], (err) => {
-        if (err) reject(`${err}`);
-        else resolve({ success: true });
-      });
+      const query =
+        "INSERT INTO students(id, major_no, name, email, psword, salt, token) VALUES(?, ?, ?, ?, ?, ?, ?);";
+      db.query(
+        query,
+        [user.id, 16, user.name, user.email, user.psword, 0, 0],
+        (err) => {
+          if (err) reject(`${err}`);
+          else resolve({ success: true });
+        }
+      );
     });
   }
 }
