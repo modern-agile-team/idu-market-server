c9b938e99992abcc5fecd37e6447765ac95c4eab
박우림 <woorimprog@gmail.com>
2021-02-17 00:54:43 +0900

    Close(woorim/login) : 로그인 API 구현 완료 #2

--- app/src/apis/root/index.js
+++ app/src/apis/root/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const ctrl = require("./root.ctrl");
 
-router.post("/session", ctrl.process.login);
+router.post("/jwt", ctrl.process.login);
 router.post("/user", ctrl.process.signup);
 router.post("/forgot-id", ctrl.process.sendEmailForId);
 router.post("/forgot-password", ctrl.process.sendEmailForPsword);
