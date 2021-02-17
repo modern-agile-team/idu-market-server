8b2861bcb173a95bfaf13b25804fe7a88f14301a
박우림 <woorimprog@gmail.com>
2021-02-18 05:03:32 +0900

    Add(woorim/findPassword) : 발급된 이메일이 접속할 UI를 위한 기본 설정 추가 #5

--- app/app.js
+++ app/app.js
@@ -7,11 +7,16 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
-const root = require("./src/apis/root");
-
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const view = require("./src/apis/view");
+const root = require("./src/apis/root");
+
+app.use("/", view);
 app.use("/api/", root);
 
 module.exports = app;
