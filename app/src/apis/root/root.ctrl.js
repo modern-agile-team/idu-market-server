e1303f7a5eded1e316a4081b92742c9993282cf1
박우림 <woorimprog@gmail.com>
2021-02-17 18:50:24 +0900

    Implement(woorim/findID) : ID 이메일 전송 라우트의 컨트롤러 구현

--- app/src/apis/root/root.ctrl.js
+++ app/src/apis/root/root.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const User = require("../../models/services/User");
+const Email = require("../../models/services/Email");
 
 const process = {
   login: async (req, res) => {
@@ -15,7 +16,12 @@ const process = {
     if (response.success) return res.status(201).json(response);
     return res.status(409).json(response);
   },
-  sendEmailForId: async () => {},
+  sendEmailForId: async (req, res) => {
+    const email = new Email(req.body);
+    const response = await email.sendId();
+    if (response.success) return res.status(200).json(response);
+    return res.status(409).json(response);
+  },
   sendEmailForPsword: async () => {},
   resetPsword: async () => {},
 };
