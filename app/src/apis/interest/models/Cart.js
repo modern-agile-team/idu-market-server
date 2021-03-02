92611a41f95fcf7d3e45b0de9b9e0b8bc9747f29
kimjiwon-98 <jiwonpg98@gmail.com>
2021-03-02 17:31:18 +0900

    Refactor(jiwon/select): select db query문 변경

--- app/src/apis/interest/models/Cart.js
+++ app/src/apis/interest/models/Cart.js
@@ -1,4 +1,4 @@
-const CartStorage = require("./CartStorage");
+const InterestProductStorage = require("./InterestProductStorage");
 
 class Cart {
   constructor(body) {
@@ -8,17 +8,18 @@ class Cart {
   async product() {
     const product = this.body;
     try {
-      const response = await CartStorage.existCart(product);
-      return response;
+      const response = await InterestProductStorage.existCart(product);
+      if (response) return { success: true, msg: "장바구니에 저장" };
+      return { success: false, msg: "이미 장바구니에 저장" };
     } catch (err) {
-      return { success: false, msg: "값을 받아오지 못함" };
+      return { success: false, msg: "boards 데이터베이스에 존재하지 않음" };
     }
   }
 
   async shoppingBasket() {
     const studentId = this.body;
     try {
-      const response = await CartStorage.showProduct(studentId);
+      const response = await InterestProductStorage.showProduct(studentId);
       return response;
     } catch (err) {
       return { success: false, msg: "select 실패" };
@@ -28,8 +29,9 @@ class Cart {
   async productList() {
     const product = this.body;
     try {
-      const response = await CartStorage.removeList(product);
-      return response;
+      const response = await InterestProductStorage.remove(product);
+      if (response) return { success: true, msg: "정상적으로 삭제" };
+      return { success: false, msg: "database에 존재하지 않는다." };
     } catch (err) {
       return { success: false, msg: "삭제 실패" };
     }
--- app/src/apis/interest/models/InterestProductStorage.js
+++ app/src/apis/interest/models/InterestProductStorage.js
@@ -1,25 +1,19 @@
 const db = require("../../../config/db");
 
-class CartStorage {
-  //코드 번호
-  static boardCode(code) {
-    if (code === 1) return "무료나눔";
-    if (code === 2) return "교재";
-    if (code === 3) return "IT기기";
-    if (code === 4) return "관리자 공지";
-  }
+class InterestProductStorage {
   //장바구니 화면
   static showProduct(id) {
     return new Promise((resolve, reject) => {
-      const category = [];
-      const boardTitle = "SELECT title FROM boards WHERE shopping_basket.board_no = no";
-      const sql = `SELECT student_id, board_code_no, (${boardTitle}) AS board_title FROM shopping_basket WHERE student_id = ?`;
+      const sql = ` SELECT bc.name, bo.title, sb.student_id 
+      FROM shopping_basket sb 
+      JOIN boards bo 
+      ON bo.no = sb.board_no 
+      JOIN board_codes bc 
+      ON bc.no = sb.board_code_no 
+      WHERE sb.student_id = ?`;
       db.query(sql, [id], (err, rows) => {
         if (err) reject(err);
-        for (let i = 0; i < rows.length; i++) {
-          category.push(this.boardCode(rows[i].board_code_no));
-        }
-        resolve({ success: true, rows: rows, category: category, msg: "정상 검색" });
+        resolve({ success: true, rows: rows, msg: "정상 검색" });
       });
     });
   }
@@ -31,35 +25,34 @@ class CartStorage {
       db.query(existCart, testParams, (err, rows) => {
         if (err) throw err;
         if (!rows.length) {
-          resolve(this.getProduct(data));
-        } else resolve({ success: false, msg: "이미 장바구니에 저장" });
+          resolve(this.save(data));
+        } else resolve(false);
       });
     });
   }
   //장바구니에 담는 코드
-  static getProduct(data) {
+  static save(data) {
     return new Promise((resolve, reject) => {
       const sql = `INSERT INTO shopping_basket(board_code_no, student_id, board_no) VALUES(?, ?, ?)`;
       const params = [data.board_code_no, data.student_id, data.board_no];
-      console.log(data);
       db.query(sql, params, (err, rows) => {
-        if (err) reject({ success: false });
-        resolve({ success: true, msg: "장바구니에 저장되었습니다" });
+        if (err) reject(false);
+        resolve(true);
       });
     });
   }
 
   //장바구니 있는 물건 삭제
-  static removeList(data) {
+  static remove(data) {
     return new Promise((resolve, reject) => {
       const sql = `DELETE FROM shopping_basket WHERE board_no = ? AND student_id = ?`;
       const params = [data.board_no, data.student];
       db.query(sql, params, (err, rows) => {
-        if (err) reject({ success: false, msg: "database에 존재하지 않는다." });
-        resolve({ success: true, msg: "정상적으로 삭제되었다" });
+        if (err) reject(false);
+        resolve(true);
       });
     });
   }
 }
 
-module.exports = CartStorage;
+module.exports = InterestProductStorage;
