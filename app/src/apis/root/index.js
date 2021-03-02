a5be4533ceb0f8a7427ba350cc99444750db5d5e
JeongWonji <wonjiprog@gmail.com>
2021-03-02 17:28:29 +0900

    Add(wonji/connectSocket) : 소켓서버로의 경로 지정

--- app/src/apis/root/index.js
+++ app/src/apis/root/index.js
@@ -14,3 +14,10 @@ router.post("/forgot-password", notLogined, ctrl.process.sendEmailForPsword);
 router.patch("/password", notLogined, ctrl.process.resetPsword);
 
 module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+const ctrl = require("./root.ctrl");
+
+router.get("/", ctrl.process.server);
+
