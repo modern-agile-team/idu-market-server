9234d8cf9171ad564c38bb37de550080413bd2ff
kimjiwon-98 <jiwonpg98@gmail.com>
2021-03-09 15:26:14 +0900

    Refactor(jiwon/basket): 관심목록 경로 수정 #19

--- app/src/models/services/watchlist/WatchListStorage.js
+++ app/src/models/services/watchlist/WatchListStorage.js
@@ -4,12 +4,12 @@ class WatchListStorage {
   //장바구니 화면
   static findAllById(id) {
     return new Promise((resolve, reject) => {
-      const sql = ` SELECT bc.name AS codename, bo.title, wl.student_id, bo.student_id AS seller
-      FROM watch_list wl 
+      const sql = ` SELECT cat.name AS category, bo.title, wl.student_id, bo.student_id AS seller
+      FROM watch_lists wl 
       JOIN boards bo 
       ON bo.no = wl.board_no 
-      JOIN board_codes bc 
-      ON bc.no = wl.board_code_no 
+      JOIN categories cat
+      ON bc.no = wl.category_no 
       WHERE wl.student_id = ?`;
       db.query(sql, [id], (err, watchLists) => {
         if (err) reject(err);
@@ -20,7 +20,7 @@ class WatchListStorage {
   //장바구니 담는 코드
   static isexist(cilent) {
     return new Promise((resolve, reject) => {
-      const isexist = `SELECT board_no, student_id FROM watch_list WHERE board_no=? AND student_id=?`;
+      const isexist = `SELECT board_no, student_id FROM watch_lists WHERE board_no=? AND student_id=?`;
       const testParams = [cilent.boardNum, cilent.studentId];
       db.query(isexist, testParams, (err, rows) => {
         if (err) reject(err);
@@ -33,8 +33,8 @@ class WatchListStorage {
   //장바구니에 담는 코드
   static update(cilent) {
     return new Promise((resolve, reject) => {
-      const sql = `INSERT INTO watch_list(board_no, board_code_no, student_id) VALUES(?, ?, ?)`;
-      const params = [cilent.boardNum, cilent.boardCodeNum, cilent.studentId];
+      const sql = `INSERT INTO watch_lists(board_no, category_no, student_id) VALUES(?, ?, ?)`;
+      const params = [cilent.boardNum, cilent.categoryNum, cilent.studentId];
       db.query(sql, params, (err) => {
         if (err) reject(false);
         resolve(true);
@@ -45,7 +45,7 @@ class WatchListStorage {
   //장바구니 있는 물건 삭제
   static delete(cilent) {
     return new Promise((resolve, reject) => {
-      const sql = `DELETE FROM watch_list WHERE board_no = ? AND student_id = ?`;
+      const sql = `DELETE FROM watch_lists WHERE board_no = ? AND student_id = ?`;
       const params = [cilent.boardNum, cilent.studentId];
       db.query(sql, params, (err) => {
         if (err) reject(false);
