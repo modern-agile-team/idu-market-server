9cbab4d3ca06a6a637ab89af7c131cc66e550774
JeongWonji <wonjiprog@gmail.com>
2021-03-10 11:27:36 +0900

    Fix(wonji/salelists): 에러발생시 resolve되는 에러 해결

--- app/src/models/services/saleList/SaleListStorage.js
+++ app/src/models/services/saleList/SaleListStorage.js
@@ -4,7 +4,7 @@ const db = require("../../../config/db");
 
 class SaleListStorage {
   //판매목록 화면
-  static find(id) {
+  static findAllById(id) {
     return new Promise((resolve, reject) => {
       const sql = ` SELECT sa.student_id, bo.title
           FROM sale_lists sa 
@@ -13,17 +13,17 @@ class SaleListStorage {
           WHERE sa.student_id = ?`;
       db.query(sql, [id], (err, saleList) => {
         if (err) reject(err);
-        resolve({ success: true, saleList: saleList });
+        else resolve(saleList);
       });
     });
   }
 
-  static isexist(client) {
+  static isExist(client) {
     return new Promise((resolve, reject) => {
-      const isexist = `SELECT board_no, student_id FROM sale_lists WHERE board_no=? AND student_id=?`;
+      const isExist = `SELECT board_no, student_id FROM sale_lists WHERE board_no=? AND student_id=?`;
       const testParams = [client.boardNum, client.studentId];
-      db.query(isexist, testParams, (err, rows) => {
-        if (err) throw err;
+      db.query(isExist, testParams, (err, rows) => {
+        if (err) reject(err);
         if (!rows.length) {
           resolve(true);
         } else resolve(false);
@@ -36,8 +36,8 @@ class SaleListStorage {
       const sql = `INSERT INTO sale_lists(board_no, category_no, student_id) VALUES(?, ?, ?)`;
       const params = [client.boardNum, client.categoryNum, client.studentId];
       db.query(sql, params, (err, rows) => {
-        if (err) reject(false);
-        resolve(true);
+        if (err) reject(err);
+        else resolve(true);
       });
     });
   }
