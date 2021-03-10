1b20ec5302763d20ce22c098d6fb5bcf08965662
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-10 13:38:02 +0900

    Add(jiwon/image): s3.json삭제 및 .env에 키값 추가 #55

--- app/src/middlewares/upload.js
+++ app/src/middlewares/upload.js
@@ -1,15 +1,18 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const aws = require("aws-sdk");
-aws.config.loadFromPath(__dirname + "/../../s3.json");
 
-const s3 = new aws.S3();
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_PW,
+});
 const upload = multer(
   {
     storage: multerS3({
       s3: s3,
       bucket: "woowahan-agile",
       acl: "public-read",
+      region: "ap-northeast-2",
       key: function (req, file, cb) {
         cb(null, Date.now() + "." + file.originalname.split(".").pop());
       },
