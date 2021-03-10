854a0fa6d586a683cc5220e057535a501ffcf4cc
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-10 13:52:10 +0900

    Add(jiwon/image): cors 라이브러리 설치 및 filesize limit 설정#55

--- app/src/middlewares/upload.js
+++ app/src/middlewares/upload.js
@@ -3,23 +3,21 @@ const multerS3 = require("multer-s3");
 const aws = require("aws-sdk");
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ID,
-  secretAccessKey: process.env.AWS_PW,
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
 });
-const upload = multer(
-  {
-    storage: multerS3({
-      s3: s3,
-      bucket: "woowahan-agile",
-      acl: "public-read",
-      region: "ap-northeast-2",
-      key: function (req, file, cb) {
-        cb(null, Date.now() + "." + file.originalname.split(".").pop());
-      },
-    }),
-  },
-  "NONE"
-);
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "woowahan-agile",
+    acl: "public-read",
+    region: "ap-northeast-2",
+    key: function (req, file, cb) {
+      cb(null, Date.now() + "." + file.originalname.split(".").pop());
+    },
+  }),
+  limits: { fieldSize: 100 * 1024 * 1024 },
+});
 
 // const deleteImage = (path) => {
 //   return s3.deleteObject({
