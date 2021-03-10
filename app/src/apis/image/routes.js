a7952518085728f796c4de918d6ccec57bae7003
JeongWonji <wonjiprog@gmail.com>
2021-03-10 13:19:57 +0900

    Delete(woni/purchaselists): 커밋오류 해결을 위한 app폴더 삭제

--- app/src/apis/image/routes.js
+++
@@ -1,7 +0,0 @@
-const upload = require("../../middlewares/upload");
-const express = require("express");
-const router = express.Router();
-const ctrl = require("./image.ctrl");
-
-router.post("/", upload.array("image", 10), ctrl.process.upload);
-module.exports = router;
--- app/src/apis/view/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/reset/password/:token", (req, res) => {
-  res.render("reset-password");
-});
-
-module.exports = router;
