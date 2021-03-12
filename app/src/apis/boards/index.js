81288122c2963db89d0109a29f23f8dafb93004b
박우림 <woorimprog@gmail.com>
2021-03-12 10:50:03 +0900

    Add(woorim/boards): 게시판 조회수 API 라우트 추가

--- app/src/apis/boards/index.js
+++ app/src/apis/boards/index.js
@@ -9,6 +9,7 @@ router.get("/:categoryName", ctrl.process.findAllByCategoryName);
 router.get("/:categoryName/:num", ctrl.process.detailFindOneByCategoryName);
 
 router.put("/:categoryName/:num", ctrl.process.updateByNo);
+router.patch("/:categoryName/:num", ctrl.process.updateOnlyHit);
 
 router.delete("/:categoryName/:num", ctrl.process.deleteByNo);
 
