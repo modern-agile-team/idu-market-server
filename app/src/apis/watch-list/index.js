fe6196e55ebca383c71fb91b46464d515fb5e40b
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-23 17:46:35 +0900

    Refactor(jiwon/watchlist): watchlist api url 변경 #19

--- app/src/apis/watch-list/index.js
+++ app/src/apis/watch-list/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require("./watch-list.ctrl");
 
 router.get("/:studentId", ctrl.output.findAllByStudentId);
-router.delete("/:studentId/delete", ctrl.process.delete);
-router.post("/:studentId/add", ctrl.process.update);
+router.delete("/:studentId", ctrl.process.delete);
+router.post("/:studentId", ctrl.process.update);
 
 module.exports = router;
