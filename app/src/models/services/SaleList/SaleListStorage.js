e3e5ebee0fc9a0d98227c535294550d41f24a728
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-27 20:45:00 +0900

    Fix(jiwon/salelist): 조회 query문 오류 해결 #60

--- app/src/models/services/SaleList/SaleListStorage.js
+++ app/src/models/services/SaleList/SaleListStorage.js
@@ -6,7 +6,7 @@ class SaleListStorage {
   //판매목록 화면
   static findAllByStatus(id) {
     return new Promise((resolve, reject) => {
-      const sql = ` SELECT bo.no AS num, bo.student_id AS studentId, bo.thumbnail, bo.title, bo.hit, 
+      const sql = `SELECT bo.no AS num, bo.student_id AS studentId, bo.thumbnail, bo.title, bo.hit, 
       bo.price, cat.name AS categoryName, COUNT(cmt.content) AS commentCount
       ,date_format(bo.in_date, '%Y-%m-%d %H:%i:%s') AS inDate
       FROM boards bo
@@ -14,7 +14,9 @@ class SaleListStorage {
       ON cat.no = bo.category_no
       LEFT JOIN comments cmt
       ON cmt.board_no = bo.no
-      WHERE status = 2 AND student_id = ?`;
+      WHERE status = 2 AND bo.student_id = ?
+      GROUP BY bo.no
+      ORDER BY bo.no desc`;
 
       db.query(sql, [id], (err, saleLists) => {
         if (err) reject(err);
