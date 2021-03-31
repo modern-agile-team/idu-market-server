f64832d3b54ca4ce1cd8567b54f4a0e748d8ac29
박우림 <woorimprog@gmail.com>
2021-03-31 16:26:22 +0900

    Fix(woorim/boards): 닉네임 반환 쿼리 수정

--- app/src/models/services/Board/BoardStorage.js
+++ app/src/models/services/Board/BoardStorage.js
@@ -38,7 +38,7 @@ class BoardStroage {
     }
 
     return new Promise((resolve, reject) => {
-      const query = `SELECT bo.no AS num, st.ninkname AS ninkname, st.profile_path AS profilePath, bo.thumbnail, bo.title, bo.hit, bo.price, bo.status,
+      const query = `SELECT bo.no AS num, st.nickname AS nickname, st.profile_path AS profilePath, bo.thumbnail, bo.title, bo.hit, bo.price, bo.status,
       date_format(bo.in_date, '%Y-%m-%d %H:%i:%s') AS inDate,
       COUNT(cmt.content) AS commentCount
       FROM boards AS bo
