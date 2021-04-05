b5b2ac7d970470033d7fe631ad34d9e309e9f6ad
박우림 <woorimprog@gmail.com>
2021-04-05 18:03:33 +0900

    Fix(woorim/TDD): URL 경로에 하이픈 들어가는 것 문자열 백틱으로 처리

--- app/src/tests/saleList.spec.js
+++ app/src/tests/saleList.spec.js
@@ -5,7 +5,7 @@ const server = request(app);
 describe("판매 목록", () => {
   it("GET 판매 목록 조회 시 200 반환", async () => {
     try {
-      const res = await server.get("/api/sale-list/test");
+      const res = await server.get(`/api/sale-list/test`);
 
       expect(res.statusCode).toEqual(200);
     } catch (err) {
