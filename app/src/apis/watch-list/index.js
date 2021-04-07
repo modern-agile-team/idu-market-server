e5b04644e00b975cf153228d9bd78c933b00f2bb
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-04-07 16:09:34 +0900

    Add(jiwon/watchlist): 관심목록api ts변환 #19

--- app/src/apis/watch-list/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("./watch-list.ctrl");
-
-router.get("/:studentId", ctrl.process.findAllByStudentId);
-router.delete("/:studentId", ctrl.process.delete);
-router.post("/:studentId", ctrl.process.update);
-
-module.exports = router;
