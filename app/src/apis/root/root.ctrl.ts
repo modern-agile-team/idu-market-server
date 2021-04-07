6cfd361d2f8d43cf413f0623798129f44658d9cd
박우림 <woorimprog@gmail.com>
2021-04-07 18:20:28 +0900

    Modify(woorim/typescript): 초기 타스 변환

--- app/src/apis/root/root.ctrl.ts
+++ app/src/apis/root/root.ctrl.ts
@@ -3,24 +3,22 @@ import logger from "../../config/logger";
 
 import Student from "../../models/services/Student/Student";
 import Email from "../../models/services/Email/Email";
+import Error from "../../models/utils/Error";
 
 const auth = {
-  resAuthorizedUserInfo: (req: Request, res: Response) => {
+  resAuthorizedStudentInfo: (req: Request, res: Response) => {
     // 인증 미들웨어를 통과했으므로 인증된 사용자이다.
     // 따라서 유저 정보를 응답한다.
-    const user = req.user;
-    delete user.iat;
-    delete user.exp;
-    delete user.iss;
+    const auth = req.auth;
 
     res.status(200).json({
       success: true,
       msg: "로그인된 사용자입니다. 서비스 이용이 가능합니다.",
-      user,
+      auth,
     });
   },
 
-  resUnAuthorizedInfo: (req, res) => {
+  resUnAuthorizedInfo: (req: Request, res: Response) => {
     // 비인증 미들웨어를 통과했으므로 비인증된 사용자이다.
     res.status(200).json({
       success: true,
@@ -30,10 +28,10 @@ const auth = {
 };
 
 const process = {
-  login: async (req, res) => {
+  login: async (req: Request, res: Response): Promise<Response> => {
     try {
-      const user = new User(req);
-      const response = await user.login();
+      const student = new Student(req);
+      const response = await student.login();
       if (response.success) {
         logger.info(`POST /api/jwt 201 ${response.msg}`);
         return res.status(201).json(response);
@@ -47,10 +45,10 @@ const process = {
     }
   },
 
-  signup: async (req, res) => {
+  signup: async (req: Request, res: Response): Promise<Response> => {
     try {
-      const user = new User(req);
-      const response = await user.signup();
+      const student = new Student(req);
+      const response = await student.signup();
       if (response.success) {
         logger.info(`POST /api/student 201 ${response.msg}`);
         return res.status(201).json(response);
@@ -64,7 +62,7 @@ const process = {
     }
   },
 
-  sendEmailForId: async (req, res) => {
+  sendEmailForId: async (req: Request, res: Response): Promise<Response> => {
     try {
       const email = new Email(req);
       const response = await email.sendId();
@@ -81,7 +79,10 @@ const process = {
     }
   },
 
-  sendEmailForPsword: async (req, res) => {
+  sendEmailForPsword: async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
     try {
       const email = new Email(req);
       const response = await email.sendLinkForPsword();
@@ -98,10 +99,10 @@ const process = {
     }
   },
 
-  resetPsword: async (req, res) => {
+  resetPsword: async (req: Request, res: Response): Promise<Response> => {
     try {
-      const user = new User(req);
-      const response = await user.resetPassword();
+      const student = new Student(req);
+      const response = await student.resetPassword();
       if (response.success) {
         logger.info(`PATCH /api/password 200 ${response.msg}`);
         return res.status(200).json(response);
@@ -116,7 +117,7 @@ const process = {
   },
 };
 
-module.exports = {
+export default {
   auth,
   process,
 };
