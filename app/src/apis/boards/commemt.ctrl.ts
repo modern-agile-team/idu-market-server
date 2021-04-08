fefd7e41792cea1ac7786872c1c08135bed66518
JeonSoongu <soonguprog@gmail.com>
2021-04-08 11:10:35 +0900

    Refactor(soongu/typescript): 컨트롤러 리턴 타입 형 추가

--- app/src/apis/boards/commemt.ctrl.ts
+++ app/src/apis/boards/commemt.ctrl.ts
@@ -11,20 +11,27 @@ interface response {
 }
 
 const process = {
-  createByBoardNum: async (req : Request, res : Response) => {
+  createByBoardNum: async (req: Request, res: Response): Promise<Response> => {
     const comment = new Comment(req);
-    const response : response = await comment.createByBoardNum();
+    const response: response = await comment.createByBoardNum();
     if (response.success) {
       logger.info(`POST /api/boards/categoryName/num 201 ${response.msg}`);
       return res.status(201).json(response);
+    }
+    if (response.isError) {
+      logger.error(`POST /api/boards/categoryName/num 500 ${response.errMsg}`);
+      return res.status(500).json(response);
     }
     logger.error(`POST /api/boards/categoryName/num 400 ${response.msg}`);
     res.status(400).json(response);
   },
 
-  createReplyByGroupNum: async (req : Request, res : Response) => {
+  createReplyByGroupNum: async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
     const comment = new Comment(req);
-    const response : response = await comment.createReplyByGroupNum();
+    const response: response = await comment.createReplyByGroupNum();
     if (response.success) {
       logger.info(
         `POST /api/boards/categoryName/num/groupNum 201 ${response.msg}`
@@ -33,17 +40,19 @@ const process = {
     }
     if (response.isError) {
       logger.error(
-        `POST /api/boards/categoryName/num/groupNum 400 ${response.errMsg}`
+        `POST /api/boards/categoryName/num/groupNum 500 ${response.errMsg}`
       );
-      return res.status(400).json(response.clientMsg);
+      return res.status(500).json(response.clientMsg);
     }
-    logger.info(`POST /api/boards/categoryName/num/groupNum 400 ${response.msg}`);
+    logger.info(
+      `POST /api/boards/categoryName/num/groupNum 400 ${response.msg}`
+    );
     return res.status(400).json(response);
   },
 
-  updateByNum: async (req : Request, res : Response) => {
+  updateByNum: async (req: Request, res: Response): Promise<Response> => {
     const comment = new Comment(req);
-    const response : response = await comment.updateByNum();
+    const response: response = await comment.updateByNum();
     if (response.success) {
       logger.info(
         `PATCH /api/boards/categoryName/num/commentNum 200 ${response.msg}`
@@ -52,17 +61,19 @@ const process = {
     }
     if (response.isError) {
       logger.error(
-        `PATCH /api/boards/categoryName/num/commentNum 400 ${response.errMsg}`
+        `PATCH /api/boards/categoryName/num/commentNum 500 ${response.errMsg}`
       );
-      return res.status(400).json(response.clientMsg);
+      return res.status(500).json(response.clientMsg);
     }
-    logger.info(`PATCH /api/boards/categoryName/num/commentNum 400 ${response.msg}`);
+    logger.info(
+      `PATCH /api/boards/categoryName/num/commentNum 400 ${response.msg}`
+    );
     return res.status(400).json(response);
   },
 
-  deleteByNum: async (req : Request, res : Response) => {
+  deleteByNum: async (req: Request, res: Response): Promise<Response> => {
     const comment = new Comment(req);
-    let response : response = {};
+    let response: response = {};
     if (req.body.depth === 0) {
       response = await comment.deleteCommentByNum();
     } else {
@@ -76,17 +87,22 @@ const process = {
     }
     if (response.isError) {
       logger.error(
-        `DELETE /api/boards/categoryName/num/commentNum 400 ${response.errMsg}`
+        `DELETE /api/boards/categoryName/num/commentNum 500 ${response.errMsg}`
       );
-      return res.status(400).json(response.clientMsg);
+      return res.status(500).json(response.clientMsg);
     }
-    logger.info(`DELETE /api/boards/categoryName/num/commentNum 400 ${response.msg}`);
+    logger.info(
+      `DELETE /api/boards/categoryName/num/commentNum 400 ${response.msg}`
+    );
     return res.status(400).json(response);
   },
 
-  findStudentIdByNum: async (req : Request, res : Response) => {
+  findStudentIdByNum: async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
     const comment = new Comment(req);
-    const response : response = await comment.findStudentIdByNum();
+    const response: response = await comment.findStudentIdByNum();
     if (response.success) {
       logger.info(
         `GET /api/boards/categoryName/num/comments 200 ${response.msg}`
@@ -95,11 +111,13 @@ const process = {
     }
     if (response.isError) {
       logger.error(
-        `GET /api/boards/categoryName/num.comments 400 ${response.errMsg}`
+        `GET /api/boards/categoryName/num.comments 500 ${response.errMsg}`
       );
-      return res.status(400).json(response.clientMsg);
+      return res.status(500).json(response.clientMsg);
     }
-    logger.info(`GET /api/boards/categoryName/num.comments 400 ${response.msg}`);
+    logger.info(
+      `GET /api/boards/categoryName/num.comments 400 ${response.msg}`
+    );
     return res.status(400).json(response);
   },
 };
