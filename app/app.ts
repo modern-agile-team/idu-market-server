efc279e85bf6b9313e236e5278223cd375240816
박우림 <woorimprog@gmail.com>
2021-05-14 22:00:38 +0900

    Modify(woorim/mariaDB): express의 urlencoded를 기본 설정으로 변경

--- app/app.ts
+++ app/app.ts
@@ -30,7 +30,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(
