b1c3be5c0c84891768633e4544e80d9cbc5a7012
박우림 <woorimprog@gmail.com>
2021-05-16 00:50:06 +0900

    Modify(woorim/mariaDB): 이해하기 쉬운 변수 & 함수명으로 변경

--- app/src/apis/watch-list/index.ts
+++ app/src/apis/watch-list/index.ts
@@ -4,7 +4,7 @@ import ctrl from "./watch-list.ctrl";
 const router: Router = Router();
 
 router.get("/:studentId", ctrl.findAllByStudentId);
+router.post("/:studentId", ctrl.create);
 router.delete("/:studentId", ctrl.delete);
-router.post("/:studentId", ctrl.update);
 
 export default router;
