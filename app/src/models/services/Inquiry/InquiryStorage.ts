8d47e6aabcb70e469f84e54c96b728a06efabd73
jiwonpg98 <jiwonpg98@gmail.com>
2021-05-19 14:13:17 +0900

    Add(jiwon/inquiry): db에 문의사항 내용 저장 #188

---
+++ app/src/models/services/Inquiry/InquiryStorage.ts
@@ -0,0 +1,32 @@
+import mariadb from "../../../config/mariadb";
+
+interface body {
+  studentId: string;
+  title: string;
+  content: string;
+}
+
+class InquiryStorage {
+  static async create(body: body): Promise<boolean> {
+    let conn;
+    try {
+      conn = await mariadb.getConnection();
+      const query = `INSERT INTO inquirys(student_id, title, content) VALUES (?, ?, ?)`;
+
+      const inquiry = await conn.query(query, [
+        body.studentId,
+        body.title,
+        body.content,
+      ]);
+
+      if (inquiry.insertId) return true;
+      return false;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+}
+
+export default InquiryStorage;
