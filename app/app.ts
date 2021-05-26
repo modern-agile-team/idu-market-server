c5564a1dff58ef9ce9d370f1fcbaad1b9d2aa0e8
박우림 <woorimprog@gmail.com>
2021-05-26 17:09:05 +0900

    Add(woorim/develop): CORS 적용

--- app/app.ts
+++ app/app.ts
@@ -30,8 +30,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(corsOptionsDelegate));
-app.use(cors());
+app.use(cookieParser());
+app.use(cors(corsOptionsDelegate));
 app.use(
   morgan("tiny", {
     stream: {
