b68f9e9202aa344091eebef04c124747dba1850a
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-07-05 20:56:27 +0900

    Refactor(jiwon/image): s3관련 image 코드 숨기기 #55

--- app/src/apis/image/index.ts
+++ app/src/apis/image/index.ts
@@ -1,10 +1,9 @@
-import { upload } from "../../middlewares/s3";
 import * as express from "express";
 import ctrl from "./image.ctrl";
 
 const router: express.Router = express.Router();
 
-router.post("/", upload.array("files", 5), ctrl.upload);
+// router.post("/", upload.array("files", 5), ctrl.upload);
 // router.delete("/", ctrl.delete);
 
 export default router;
