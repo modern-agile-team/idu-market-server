cdae07c3dc33839cb36e65840490b19013c7b3be
박우림 <woorimprog@gmail.com>
2021-07-08 20:17:59 +0900

    Modify(woorim/apiSecure): 판매목록 API에 API 인증 미들웨어 등록 #240

--- app/src/apis/sale-list/index.ts
+++ app/src/apis/sale-list/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { apiAuth } from "../../middlewares/apiAuth";
 import ctrl from "./sale-list.ctrl";
 
 const router: Router = Router();
 
-router.get("/:studentId", ctrl.read);
+router.get("/:studentId", apiAuth, ctrl.read);
 
 export default router;
