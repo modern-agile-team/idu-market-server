67010e55cb38a815f2fe7f5503b393a7794e2825
박우림 <woorimprog@gmail.com>
2021-07-08 20:10:48 +0900

    Modify: 게시판에 API 인증 미들웨어 등록 #240

--- app/src/apis/boards/index.ts
+++ app/src/apis/boards/index.ts
@@ -1,34 +1,51 @@
 import * as express from "express";
-const router: express.Router = express.Router();
+import { apiAuth } from "../../middlewares/apiAuth";
 
 import * as boardCtrl from "./board.ctrl";
 import * as commentCtrl from "./commemt.ctrl";
 
-router.post("/:categoryName", boardCtrl.process.createByCategoryName);
-router.post("/:categoryName/:num", commentCtrl.process.createByBoardNum);
+const router: express.Router = express.Router();
+
+router.post("/:categoryName", apiAuth, boardCtrl.process.createByCategoryName);
+router.post(
+  "/:categoryName/:num",
+  apiAuth,
+  commentCtrl.process.createByBoardNum
+);
 router.post(
   "/:categoryName/:num/:groupNum",
   commentCtrl.process.createReplyByGroupNum
 );
 
-router.get("/:categoryName", boardCtrl.process.findAllByCategoryNum);
+router.get("/:categoryName", apiAuth, boardCtrl.process.findAllByCategoryNum);
 router.get(
   "/:categoryName/:num/comments",
+  apiAuth,
   commentCtrl.process.findStudentIdByNum
 );
-router.get("/:categoryName/:num/:studentId", boardCtrl.process.findOneByNum);
+router.get(
+  "/:categoryName/:num/:studentId",
+  apiAuth,
+  boardCtrl.process.findOneByNum
+);
 
-router.put("/:categoryName/:num", boardCtrl.process.updateByNum);
-router.patch("/:categoryName/:num", boardCtrl.process.updateOnlyHit); // 조회수 1 증가 API
-router.patch("/:categoryName/:num/status", boardCtrl.process.updateOnlyStatus); // 상태 플래그(판매중, 예약중, 거래완료 등)만 UPDATE
+router.put("/:categoryName/:num", apiAuth, boardCtrl.process.updateByNum);
+router.patch("/:categoryName/:num", apiAuth, boardCtrl.process.updateOnlyHit); // 조회수 1 증가 API
+router.patch(
+  "/:categoryName/:num/status",
+  apiAuth,
+  boardCtrl.process.updateOnlyStatus
+); // 상태 플래그(판매중, 예약중, 거래완료 등)만 UPDATE
 router.patch(
   "/:categoryName/:num/:commentNum",
+  apiAuth,
   commentCtrl.process.updateByNum
 );
 
-router.delete("/:categoryName/:num", boardCtrl.process.deleteByNum);
+router.delete("/:categoryName/:num", apiAuth, boardCtrl.process.deleteByNum);
 router.delete(
   "/:categoryName/:num/:commentNum",
+  apiAuth,
   commentCtrl.process.deleteByNum
 );
 
