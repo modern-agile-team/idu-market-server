40944a7da71faa8e8bff9aa5ba5880849b43a4b4
박우림 <woorimprog@gmail.com>
2021-07-08 20:20:01 +0900

    Modify(woorim/apiSecure): 관심목록 API에 API 인증 미들웨어 등록 #240

--- app/src/apis/watch-list/index.ts
+++ app/src/apis/watch-list/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { apiAuth } from "../../middlewares/apiAuth";
 import ctrl from "./watch-list.ctrl";
 
 const router: Router = Router();
 
-router.get("/:studentId", ctrl.findAllByStudentId);
-router.post("/:studentId", ctrl.create);
-router.delete("/:studentId", ctrl.delete);
+router.get("/:studentId", apiAuth, ctrl.findAllByStudentId);
+router.post("/:studentId", apiAuth, ctrl.create);
+router.delete("/:studentId", apiAuth, ctrl.delete);
 
 export default router;
