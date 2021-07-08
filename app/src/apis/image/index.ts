e56e73e416e097fab2866977e0a85be7217586a7
박우림 <woorimprog@gmail.com>
2021-07-08 20:12:36 +0900

    Modify(woorim/apiSecure): 이미지 API에 API 인증 미들웨어 등록 #240

--- app/src/apis/image/index.ts
+++ app/src/apis/image/index.ts
@@ -1,10 +1,13 @@
-import { upload } from "../../middlewares/s3";
 import * as express from "express";
+
+import { upload } from "../../middlewares/s3";
+import { apiAuth } from "../../middlewares/apiAuth";
+
 import ctrl from "./image.ctrl";
 
 const router: express.Router = express.Router();
 
-router.post("/", upload.array("upload", 5), ctrl.upload);
-router.delete("/", ctrl.delete);
+router.post("/", apiAuth, upload.array("upload", 5), ctrl.upload);
+router.delete("/", apiAuth, ctrl.delete);
 
 export default router;
